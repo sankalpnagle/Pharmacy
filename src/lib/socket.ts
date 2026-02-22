41d9e38d9ac6ed8845d24ee9fadcc7f045ac0825
sankalpnagle <123073869+sankalpnagle@users.noreply.github.com>
2026-02-23 05:06:17 +0530

    Simplify socket connection URL configuration
    Refactor socket initialization to use default URL.

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -2,9 +2,4 @@
 
 import { io } from "socket.io-client";
 
-const URL =
-  process.env.NODE_ENV === "production"
-    ? "https://pharmacy-lake-mu.vercel.app/"
-    : "http://localhost:5000";
-
-export const socket = io(URL);
+export const socket = io();
