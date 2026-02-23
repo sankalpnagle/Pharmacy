9f2b58dc8db7feb5e5f36ffc0a6ef86872b23104
sankalp <nagle.sankalp@gmail.com>
2026-02-23 12:19:51 +0530

    fix error

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,50 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: false,
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
-  },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
   },
+  reactStrictMode: true,
+
+  // ✅ Required only if you really need it
+  trailingSlash: false,
+
+  // ✅ Allow S3 images
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pharmacy-app-storage.s3.us-east-1.amazonaws.com",
+        hostname: "pharmacy-uploads-123.s3.ap-south-1.amazonaws.com",
         pathname: "/**",
       },
     ],
   },
+
+  // ✅ Prevent build from failing because of lint/types
   eslint: {
     ignoreDuringBuilds: true,
   },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,10 @@ import React from "react";
 import Home from "./(user)/home/page";
 import PharmacyHome from "./(pharmacy)/pharmacyHome/page";
 
-
-const page = () => {
+const Page = () => {
   const session = useSession();
   const role = session?.data?.user?.role;
   return <>{role === "PHARMACY_STAFF" ? <PharmacyHome /> : <Home />}</>;
 };
 
-export default page;
+export default Page;
